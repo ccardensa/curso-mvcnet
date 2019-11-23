77bb18b137d6bd4edd3fa71fbc2c3034d0e9082d
Cristian K. Cárdenas <Cristian K>
2019-11-23 10:54:10 -0300

    corrección ioc

---
+++ 01. Presentacion/ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using cl.cursocsharp.dominio.contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IProcesoFacturacion ProcesoFacturacion;
+        private IEjemplo Ejemplo;
+        public HomeController(IProcesoFacturacion ProcesoFacturacion, IEjemplo Ejemplo
+            )
+        {
+            this.ProcesoFacturacion = ProcesoFacturacion;
+            this.Ejemplo = Ejemplo;
+        }
+
+        public ActionResult Index()
+        {
+            ProcesoFacturacion.GuardarFactura();
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
